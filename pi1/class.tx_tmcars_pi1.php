564c3b2f0e08c16c92c8906a5016b4467e37162e
tomitzel <tomitzel@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-07 15:05:57 +0000

    Add functions getRecord, getRecords, getListView, getSingleView
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tm_cars/trunk@63248 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_tmcars_pi1.php
+++ pi1/class.tx_tmcars_pi1.php
@@ -42,6 +42,26 @@ class tx_tmcars_pi1 extends tslib_pibase {
 	var $scriptRelPath = 'pi1/class.tx_tmcars_pi1.php';	// Path to this script relative to the extension dir.
 	var $extKey        = 'tm_cars';	// The extension key.
 	var $pi_checkCHash = true;
+        var $templateCode;
+        var $javascript;
+        var $jQueryUI;
+        var $jQuery_multifile;
+        var $style;
+        var $uploadDir = 'uploads/tx_tmcars/';
+        var $fileFunctions;
+        var $jQuery_validate;
+        
+        function init() {
+            $this->pi_setPiVarDefaults();
+	    $this->pi_loadLL();
+            
+            /*
+            * Fetch the template file
+            */
+
+            $template = $this->conf['templateFile'] ? $this->conf['templateFile'] : 'EXT:tm_cars/res/template.html';
+            $this->templateCode = $this->cObj->fileResource($template);
+        }
 	
 	/**
 	 * The main method of the PlugIn
@@ -52,26 +72,104 @@ class tx_tmcars_pi1 extends tslib_pibase {
 	 */
 	function main($content, $conf) {
 		$this->conf = $conf;
-		$this->pi_setPiVarDefaults();
-		$this->pi_loadLL();
+		$this->init();
 		
 	
-		$content='
-			<strong>This is a few paragraphs:</strong><br />
-			<p>This is line 1</p>
-			<p>This is line 2</p>
-	
-			<h3>This is a form:</h3>
-			<form action="'.$this->pi_getPageLink($GLOBALS['TSFE']->id).'" method="POST">
-				<input type="text" name="'.$this->prefixId.'[input_field]" value="'.htmlspecialchars($this->piVars['input_field']).'">
-				<input type="submit" name="'.$this->prefixId.'[submit_button]" value="'.htmlspecialchars($this->pi_getLL('submit_button_label')).'">
-			</form>
-			<br />
-			<p>You can click here to '.$this->pi_linkToPage('get to this page again',$GLOBALS['TSFE']->id).'</p>
-		';
+		$content= $this->getListView();
 	
 		return $this->pi_wrapInBaseClass($content);
 	}
+        
+        function getListView() {
+            $template = $this->cObj->getSubpart($this->templateCode, '###TEMPLATE_LIST###');
+            
+            $markContentArray = array(
+                '###HEADER###' => $this->pi_getLL('header'),
+                '###PAGE###' => $this->pi_getLL('page'),
+                '###SORT_LIST###' => $this->pi_getLL('sort_list'),
+                '###BRAND###' => $this->pi_getLL('brand'),
+                '###PRICE###' => $this->pi_getLL('price'),
+                '###DETAILS###' => $this->pi_getLL('details')
+            );
+            
+            $template = $this->cObj->substituteMarkerArray($template, $markContentArray);
+            
+            $cars = $this->getRecords();
+            $subContent = '';
+            $subTemplate = $this->cObj->getSubpart($template, '###CARS###');
+            
+            foreach($cars as $car) {
+                $subMarkerArray = array(
+                    '###CAR_TITLE###' => $car['title'],
+                    '###CAR_COMMENT###' => $car['comment'],
+                    '###CAR_PRICE_INFO###' => $car['price_info'],
+                    '###CAR_FUEL_CON###' => $car['fuel_con'],
+                    '###CAR_FUEL_IN###' => $car['fuel_in'],
+                    '###CAR_FUEL_OUT###' => $car['fuel_out'],
+                    '###CAR_CO2###' => $car['co2'],
+                    '###CAR_DATEN###' => $car['daten'],
+                    '###CAR_EQUIPMENT###' => $car['equipment'],
+                    '###CAR_DESCRIPTIO###' => $car[''],
+                    '###CAR_NUMBER###' => $car['number'],
+                    '###CAR_USED###' => $car['used'] == 1 ? $this->pi_getLL('used') : $this->pi_getLL('new'),
+                    '###CAR_PRICE###' => $car['price'],
+                    '###CAR_###' => $car[''],
+                    '###CAR_###' => $car[''],
+                    '###CAR_###' => $car[''],
+                    
+                    
+                );
+                $subContent .= $this->cObj->substituteMarkerArray($subTemplate, $subMarkerArray);
+            }
+            
+            $content = $this->cObj->substituteSubpart($template, '###CARS###', $subContent);
+            return $content;
+        }
+        
+        function getSingleView($uid) {
+            $template = $this->cObj->getSubpart($this->templateCode, '###TEMPLATE_SINGLE###');
+            
+            $markContentArray = array(
+                '###HEADER###' => $this->pi_getLL('header'),
+            );
+            
+            $content = $this->cObj->substituteMarkerArray($template, $markContentArray);
+            return $content;
+        }
+        
+        function getRecords() {
+            $records = array();
+            
+            $fields = '*';
+            $table = 'tx_tmcars_vehicle';
+            $where = '1' . $this->cObj->enableFields($table);
+            
+            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+                    $fields, $table, $where
+                    );
+            
+            while($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+                $records[] = $row;
+            }
+            $GLOBALS['TYPO3_DB']->sql_free_result($res);
+            t3lib_utility_Debug::debug($records);
+            return $records;
+        }
+        
+        function getRecord($uid) {
+            $table = 'tx_tmcars_vehicle';
+            $fields = '*';
+            $where = 'uid = ' . $uid . $this->cObj->enableFields($table);
+            
+            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+                    $fields, $table, $where
+                    );
+            $row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+            $GLOBALS['TYPO3_DB']->sql_free_result($res);
+            
+            return $row;
+        }
+        
 }
 
 
